72557ef06b73a9ba274c27f094bbeae0c6f093df
lh <1542951820@qq.com>
2020-10-13 18:30:47 +0800

    【需求】设置recyclerView线性排列，并增加分割线

--- app/src/main/java/com/example/order/mvvmdemo/widget/ProvinceBindingAdapter.kt
+++ app/src/main/java/com/example/order/mvvmdemo/widget/ProvinceBindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.order.mvvmdemo.widget
 
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.order.mvvmdemo.utils.LogUtil
 
@@ -14,8 +16,9 @@ class ProvinceBindingAdapter {
         @BindingAdapter("bind:setAdapter")
         fun setAdapter(recyclerView: RecyclerView, adapter: ProvinceAdapter?) {
             LogUtil.d("setAdapter======")
+            recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
             recyclerView.adapter = adapter
+            recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL))
         }
     }
-
 }
